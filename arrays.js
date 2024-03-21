ba2e5e9f998c6ceb1835905f02e48456835a4a29
Jemimah Nthenya <jemimahogonda@gmail.com>
2024-03-21 09:10:00 +0300

    add files

---
+++ arrays.js
@@ -0,0 +1,24 @@
+//qest 2
+let mypets =["cow", "Bird", "Snake", "dog"];
+console.log(mypets.join())
+
+//  quest3
+let arra = [-5,9,5,3,2,-3,6,8,4,1]
+arra.sort();
+console.log({arr});
+
+//quest4
+let arr2 = ["apple","mango","apple","orange","mango","mango"];
+function removeDuplicates(arr2){
+    return arr2.filter((item,
+        index)=> arr4.indexOf(item)===index);
+}
+console.log(removeDuplicates(arr4));
+
+let arr = ["apple","mango","apple","orange","mango","mango"];
+let duplicates = arr.filter((item,index) => arr.indexOf(item) !== index);
+console.log({duplicates});
+
+//question5
+
+
